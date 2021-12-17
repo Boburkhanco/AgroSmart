ea9166ba803c3a3d9a4fe4216e2679d6e54caec7
Boburkhan <91623890+Boburkhanco@users.noreply.github.com>
2021-12-17 21:12:47 +0500

    New Footer, Small changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-searchBtn.addEventListener('click', e => {
-    headerMenu.classList.toggle('dNone')
-    searchPart.classList.toggle('dBlock')
-})
-menuToggler.addEventListener('click', e => {
-    menuHamburger.classList.toggle('displayBlock')
-    desktopMode.classList.toggle('displayNone')
-    mobileMode.classList.toggle('displayBlock')
-})
 
 products = [
     {
